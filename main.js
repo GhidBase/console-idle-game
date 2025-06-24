1e8dc8f9d07236338c335e9b0cf846a17b10cf45
Dylan Simon <grvdylans@gmail.com>
2025-06-24 12:12:17 -0600

    Add example function

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 console.log("game loaded");
 
+
 import { SaveController } from "./save-controller.js";
 const saveController = new SaveController();
 window.saveController = saveController;
+
+
+// this is a way you can create a function that
+// can be accesed in the console by typing "foo"
+Object.defineProperty(window, 'foo', {
+  get: function () {
+      console.log("hello world")
+  }
+});
