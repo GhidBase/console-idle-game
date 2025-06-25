cceb5333c9a7e359d2490b8fc0560e6c5af2ab44
Dylan Simon <grvdylans@gmail.com>
2025-06-25 11:46:57 -0600

    flesh out the save controller a bit

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
 console.log("game loaded");
 
-
 import { SaveController } from "./save-controller.js";
 const saveController = new SaveController();
 window.saveController = saveController;
 
-
-// this is a way you can create a function that
-// can be accesed in the console by typing "foo"
-Object.defineProperty(window, 'foo', {
-  get: function () {
-      console.log("hello world")
-  }
-});
-
-
-/*
-    ToDo:
-    - On startup, ask if the user would like to load the
-    save file in slot 1
-*/
+// console.log(saveController.getData());
--- save-controller.js
+++ save-controller.js
@@ -1,26 +1,64 @@
 export class SaveController {
     constructor(currentData = { health: 100, money: 0 }) {
         this.currentData = currentData;
-        this.startup()
+        this.defaultSaveFile = 1;
+        this.currentSaveFile = 1;
+        this.startup();
     }
 
+    /* 
+        Controller Use:
+        - startup loads the default save file
+        - loaded data is stored in this.currentData
+        - saved data is stored in localStorage
+        - updateData(key,value) - updates loaded data
+        - save() - saves loaded data to localStorage
+        - load() - replaces the loaded data with the localStorage version
+        - updateAndSaveData(key, value) - combines updating the loaded data,
+            and saving it to the localStorage. I did this to make it easier to
+            ensure that the localStorage version always matches with the
+            loaded Data
+    */
+
     startup() {
-        let defaultSave = localStorage.getItem(1);
-        if (defaultSave) {
-            console.log(defaultSave);
+        /*
+        I want this function to basically be the "Game Start" function of
+        the save controller. When you open a game it should typically open
+        up the default save file. 
+        */
+
+        this.currentSaveFile = this.defaultSaveFile;
+        this.load(this.currentSaveFile);
+        if (this.currentData) {
+            console.log(`Loading save file ${this.currentSaveFile}`);
+            // console.log(data);
         }
     }
 
-    save(file) {
-        localStorage.setItem(file, JSON.stringify(this.currentData));
+    save() {
+        localStorage.setItem(
+            this.currentSaveFile,
+            JSON.stringify(this.currentData)
+        );
     }
 
-    load(file) {
-        this.currentData = JSON.parse(localStorage.getItem(file));
+    load() {
+        this.currentData = JSON.parse(
+            localStorage.getItem(this.currentSaveFile)
+        );
         console.log(this.currentData);
     }
 
     updateData(stat, value) {
         this.currentData[stat] = value;
+    }
+
+    updateAndSaveData(stat, value) {
+        this.currentData[stat] = value;
+        this.save();
+    }
+
+    getData() {
+        return this.currentData;
     }
 }
